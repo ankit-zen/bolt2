2eaea97cf6aae63cd33ba0ad6a4d3f2f6b0c4726
ankit-zen <150040159+ankit-zen@users.noreply.github.com>
2025-02-28 12:37:49 +0530

    Initial commit from your app

---
+++ index.js
@@ -0,0 +1,7 @@
+// Welcome to the editor!
+// This is your main JavaScript file.
+console.log('Hello from the editor!');
+
+// Add your code here
+const app = document.getElementById('app');
+app.innerHTML = '<h1>Hello World!</h1>';
